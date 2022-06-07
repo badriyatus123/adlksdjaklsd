6d5d949bcda754e85f6e5710b5bfcaf0bc90e367
badriyatus123 <87899416+badriyatus123@users.noreply.github.com>
2022-06-07 15:04:45 +0700

    Ferst

---
+++ express.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+module.exports = function (conn) {
+  app.post('/', async (req, res) => {
+    const { number, message } = req.body
+    if (!number || !message) {
+      res.send({status: 'failed', error:'number and message are required'})
+    } else {
+      try {
+        await conn.sendMessage(`${number}@s.whatsapp.net`, { text: message })
+        res.send({ status: 'success' })
+      } catch (e) {
+        console.log(e)
+        res.send({ status: 'failed', error: e })
+      }
+    }
+  })
+  //PORT
+  app.listen(3000, function () {
+    console.log('listening on port 3000');
+  });
+}
---
+++ index.js
@@ -0,0 +1,40 @@
+const {
+    default: makeWASocket,
+    BufferJSON,
+    initInMemoryKeyStore,
+    DisconnectReason,
+    AnyMessageContent,
+    makeInMemoryStore,
+    useSingleFileAuthState,
+    delay
+} = require("@adiwajshing/baileys")
+const logg = require('pino')
+const clui = require('clui')
+const { Spinner } = clui
+let session = `./session1.json`
+const { state, saveState } = useSingleFileAuthState(session)
+
+const connectToWhatsApp = async () => {
+    const conn = makeWASocket({
+        printQRInTerminal: true,
+        logger: logg({ level: 'fatal' }),
+        auth: state,
+        browser: ["Jojo-Bot", "Safari", "3.0"]
+    })
+    conn.ev.on('connection.update', (update) => {
+        const { connection, lastDisconnect } = update
+        if(connection === 'close') {
+            const shouldReconnect = (lastDisconnect.error)?.output?.statusCode !== DisconnectReason.loggedOut
+            // console.log('connection closed due to ', lastDisconnect.error, ', reconnecting ', shouldReconnect)
+            // reconnect if not logged out
+            if(shouldReconnect) {
+                connectToWhatsApp()
+            }
+        } else if(connection === 'open') {
+            console.log('opened connection');
+            require('./express.js')(conn);   
+        }
+    })
+
+}
+connectToWhatsApp()
